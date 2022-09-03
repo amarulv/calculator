46ca3c31bf5662bdbacddad67141717ef1d15b0b
Eric Connell <ericconnell94@gmail.com>
2022-09-02 18:50:08 -0700

    calculator graphical elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column align-items-center bg-primary">
+      <h1>0</h1>
+      <div className="d-flex flex-row">
+        <div className="d-flex flex-column ms-2 gap-1">
+          <button className="red-btn">+/-</button>
+          <button className="red-btn">MRC</button>
+          <button className="white-btn">7</button>
+          <button className="white-btn">4</button>
+          <button className="white-btn">1</button>
+          <button className="red-btn">ON/C</button>
+        </div>
+        <div className="d-flex flex-column ms-2 gap-1">
+          <button className="red-btn">&radic;</button>
+          <button className="red-btn">M-</button>
+          <button className="white-btn">8</button>
+          <button className="white-btn">5</button>
+          <button className="white-btn">2</button>
+          <button className="white-btn">0</button>
+        </div>
+        <div className="d-flex flex-column ms-2 gap-1">
+          <button className="red-btn">%</button>
+          <button className="red-btn">M+</button>
+          <button className="white-btn">9</button>
+          <button className="white-btn">6</button>
+          <button className="white-btn">3</button>
+          <button className="white-btn">.</button>
+        </div>
+        <div className="d-flex flex-column ms-3 gap-1">
+          <button className="red-btn">&#247;</button>
+          <button className="red-btn">X</button>
+          <button className="red-btn">-</button>
+          <button className="red-btn">+</button>
+          <button className="red-btn flex-grow-1">=</button>
+        </div>
+      </div>
     </div>
   );
 }
